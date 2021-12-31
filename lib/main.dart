916ca893da3d662f92f157b49fe14bf7ccb1c13a
Matheos <matheos.asfaw@gmail.com>
2021-12-30 21:39:58 -0800

    added image

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,21 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Center(
+            child: Text('I Am Rich'),
+          ),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png'),
+          ),
+        ),
       ),
     ),
   );
